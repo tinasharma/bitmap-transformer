251ef9ab0d09b58b66f6d4d6d0e974a5838c0710
adam-james <adam.j.sulewski@gmail.com>
2015-10-29 23:43:20 -0700

    Invert transform

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs');
 var Bitmap = require(__dirname + '/lib/Bitmap');
+var transform = require(__dirname + '/lib/transform');
 
 var srcFile = process.argv[2] || 'bitmap1.bmp';
 var destFile = process.argv[3] || 'transformed_' + srcFile;
@@ -11,7 +12,10 @@ var bitmap = fs.readFileSync(srcFile);
 
 // Convert buffer into a Javascript Object
 var bitmapObj = new Bitmap(bitmap);
-console.log(bitmapObj);
+
+bitmap = transform(bitmap, bitmapObj);
+
+fs.writeFileSync(destFile, bitmap);
 // Run a transform on that Javascript Object.
 // Turn the transformed object back into a buffer.
 // Write that buffer to a new file.
